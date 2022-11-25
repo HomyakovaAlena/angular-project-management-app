067d552e22bb8422470ac124c5842b22ab34388c
Alena Homyakova <homiakovaelena@gmail.com>
2022-11-25 14:16:28 +0300

    feat: prepare for deployment

--- project-management-app/src/environments/environment.prod.ts
+++ project-management-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://pm-app-angular-backend-production.up.railway.app',
   settings: {
     auth: {
       // OAuth2 credentials
--- project-management-app/src/environments/environment.ts
+++ project-management-app/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://pm-app-angular-backend-production.up.railway.app',
   settings: {
     auth: {
       // OAuth2 credentials
