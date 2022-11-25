7df0fd0df6b703980a0499daa7d3720116f29a61
Alena Homyakova <homiakovaelena@gmail.com>
2022-11-25 15:00:27 +0300

    feat: add deployed backend link

--- project-management-app/src/environments/environment.prod.ts
+++ project-management-app/src/environments/environment.prod.ts
@@ -1,13 +1,11 @@
 export const environment = {
   production: true,
-  API_URL: 'http://localhost:3000',
+  API_URL: 'https://pm-app-angular-backend-production.up.railway.app',
   settings: {
     auth: {
-      // OAuth2 credentials
-      clientId: 'fake-client-id', // <Your auth client id here>
-      secretId: 'fake-secret-id', // <Your auth secret id here>
+      clientId: 'fake-client-id',
+      secretId: 'fake-secret-id',
 
-      // keys to store tokens at local storage
       accessTokenKey: 'DoPS3ZrQjM',
       refreshTokenKey: 'nmlP8PW2nb',
     },
